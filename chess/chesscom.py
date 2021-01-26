a0cb36fc8f708f87365c1f0a89c4fbcdbe743f0c
TheVerl <verofficial@protonmail.com>
2021-01-26 16:59:41 +1100

    Implemented Lichess user look up

--- chess/chesscom.py
+++ chess/chesscom.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+
+import chessdotcom
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Import modules
 import discord 
 from discord.ext import commands
-import sys
+import sys, json, datetime
 
 # Import scripts
 sys.path.insert(0, 'chess/')
@@ -30,9 +30,34 @@ async def testCommand(ctx):
 
 @bot.command(name="lichessuser")
 async def getLichessUser(ctx, username):
-    embed = discord.Embed(title="JohnSmith#0000\nRating: 9999\nRank: #1")
-    await ctx.send(embed=embed)
-    #await ctx.channel.send(lichess.getUser(username))
+    try:
+        data = (lichess.getUser(username))
+    except:
+        embed = discord.Embed(title="Sorry!", description="Could not find the specified user.")
+        await ctx.channel.send(embed=embed)
+        return
+    print(data)
+    print("\n")
+    try:
+        if (data['closed'] == True):
+            embed = discord.Embed(title="Sorry!", description="The specified user's account has been closed.")
+            await ctx.channel.send(embed=embed)
+            return
+    except:
+        embed = discord.Embed(title=data["username"] + "'s Lichess account")
+        embed.add_field(name="Bullet", value = data["perfs"]["bullet"]["rating"], inline=True)
+        embed.add_field(name="Blitz", value = data["perfs"]["blitz"]["rating"], inline=True)
+        embed.add_field(name="Rapid", value = data["perfs"]["rapid"]["rating"], inline=True)
+        embed.add_field(name="All matches", value = data['count']['all'], inline=True)
+        embed.add_field(name="Rated matches", value = data['count']['rated'], inline=True)
+        embed.add_field(name="Drawn matches", value=data['count']['draw'], inline=True)
+        embed.add_field(name="Wins", value = data['count']['win'], inline=True)
+        embed.add_field(name="Losses", value = data['count']['loss'], inline=True)
+        embed.add_field(name="W/L Ratio", value = round(data['count']['win'] / data['count']['loss'], 2), inline=True)
+        embed.add_field(name="Account creation date", value = data['createdAt'].strftime("%Y-%m-%d %H:%M"), inline=True)
+        embed.add_field(name="Last seen", value = data['seenAt'].strftime("%Y-%m-%d %H:%M"), inline=True)
+        embed.set_footer(text="Dates and times are in GMT.")
+        await ctx.channel.send(embed=embed)
 
 def init():
     with open("discord.token") as f:
